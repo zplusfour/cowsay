06131043544903c67edfcaa808fb2cfbbf158904
Zuhdi Ghawi <zgjoniur@gmail.com>
2021-02-11 07:28:46 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "strings"
+  "io/ioutil"
+  "fmt"
+  "os"
+)
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func main() {
+  if len(os.Args) < 2 {
+    fmt.Println("Insert your text.")
+  } else {
+    data, err := ioutil.ReadFile("cowsay.txt")
+    check(err)
+
+    fmt.Println(strings.ReplaceAll(string(data), "xxxxx", os.Args[1]))
+  }
+}
